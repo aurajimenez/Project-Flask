704a92917950e8155d849500c0e3cf06c53840e5
Aura Jimenez <auracristina.jimenezg@gmail.com>
2020-10-05 21:49:19 -0500

    Desarrollo hasta tests

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from  flask import Flask, request, make_response, redirect, render_template
-#Instancia
+from  flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import unittest
+
+#Instancias
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SECRETISIMA'
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    boton = SubmitField('Login')
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -12,17 +31,29 @@ def not_found(error):
 def index():
     user_ip = request.remote_addr # puede ver la ip del usuario que visita la pagina
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip',user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET','POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip' : user_ip, 
         'todos': todos,
+        'login_form': login_form,
+        'username': username,
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
 
